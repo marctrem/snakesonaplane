22b48139b1f8af6d8e8f09fae5559b771ac44722
Frédéric Vachon <fredvac@gmail.com>
2015-12-18 21:16:17 -0500

    Get rid of useless import in MoveAlgorithm interface

--- src/main/java/com/snakesonaplane/jeu/movealgos/MoveAlgorithm.java
+++ src/main/java/com/snakesonaplane/jeu/movealgos/MoveAlgorithm.java
@@ -1,7 +1,5 @@
 package com.snakesonaplane.jeu.movealgos;
 
-import com.snakesonaplane.jeu.BoardCell;
-
 
 public interface MoveAlgorithm {
     long getMove(long currentCell, long boardSize, long diceRoll);
